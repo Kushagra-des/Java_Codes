c287436d99b0aa83ce9824195897f9cdc1c02e71
Kushagra Sharma <kushagra.sharma595@gmail.com>
2026-08-16 10:44:16 +0530

    Added Files

---
+++ Lab4/Question2/Q2.java
@@ -0,0 +1,20 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        super.sound();
+        System.out.println("Dog barks");
+    }
+}
+
+public class Q2 {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}
