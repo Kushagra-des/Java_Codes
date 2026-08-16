2c8295be1a71bab046a1fb1e332bc4b166c74735
Kushagra Sharma <kushagra.sharma595@gmail.com>
2026-08-16 10:27:27 +0530

    Added Files

---
+++ TestPolymorphism.java
@@ -0,0 +1,19 @@
+public class Mother {
+    void show() {
+        System.out.println("Hello World");
+    }
+}
+
+class Child extends Mother {
+    @Override
+    void show() {
+        System.out.println("Hello JUET");
+    }
+}
+
+public class TestPolymorphism {
+    public static void main(String[] args) {
+        Mother m1 = new Child();  
+        m1.show();                 
+    }
+}
